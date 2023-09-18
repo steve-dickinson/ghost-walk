c827fe51fd099f3e4ac1503618940eba27bf290d
steve-dickinson <steve.dickinson@defra.gov.uk>
2023-09-18 19:22:44 +0100

    update long lat

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ function main() {
     const geom = new THREE.BoxGeometry(20, 20, 20);
     const mtl = new THREE.MeshBasicMaterial({color: 0xff0000});
     const box = new THREE.Mesh(geom, mtl);
+
+    // Change this to a location 0.001 degrees of latitude north of you, so that you will face it
     arjs.add(box, -0.72, 51.051); 
 
-
-    arjs.fakeGps(-0.72, 51.05);
+    // Start the GPS
+    arjs.startGps();
 
     requestAnimationFrame(render);
 
