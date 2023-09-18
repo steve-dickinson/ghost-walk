d4a555b8947d50a0b25449c38b6fd0f2bc84063f
steve-dickinson <steve.dickinson@defra.gov.uk>
2023-09-18 18:38:15 +0100

    update long lat

--- basic.js
+++ basic.js
@@ -5,26 +5,45 @@ window.onload = () => {
 
   el.addEventListener("gps-camera-update-position", async(e) => {
       if(!downloaded) {
-        alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-          const west = e.detail.position.longitude - 0.01,
-                east = e.detail.position.longitude + 0.01,
-                south = e.detail.position.latitude - 0.01;
-                north = e.detail.position.latitude + 0.01;
+          alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
+          const west = e.detail.position.longitude - 0.05,
+                east = e.detail.position.longitude + 0.05,
+                south = e.detail.position.latitude - 0.05;
+                north = e.detail.position.latitude + 0.05;
+          console.log(`${west} ${south} ${east} ${north}`);
           const response = await fetch(`https://hikar.org/webapp/map?bbox=${west},${south},${east},${north}&layers=poi&outProj=4326`);
           const pois = await response.json();
           pois.features.forEach ( feature => {
-              const entity = document.createElement("a-box");
-              entity.setAttribute("scale", {
-                  x: 20, 
+              const compoundEntity = document.createElement("a-entity");
+              compoundEntity.setAttribute('gps-new-entity-place', {
+                  latitude: feature.geometry.coordinates[1],
+                  longitude: feature.geometry.coordinates[0]
+              });
+              const box = document.createElement("a-box");
+              box.setAttribute("scale", {
+                  x: 20,
                   y: 20,
                   z: 20
               });
-              entity.setAttribute('material', { color: 'red' } );
-              entity.setAttribute('gps-new-entity-place', {
-                  latitude: feature.geometry.coordinates[1],
-                  longitude: feature.geometry.coordinates[0]
-              }); 
-              document.querySelector("a-scene").appendChild(entity);
+              box.setAttribute('material', { color: 'red' } );
+              box.setAttribute("position", {
+                  x : 0,
+                  y : 20,
+                  z: 0
+              } );
+              const text = document.createElement("a-text");
+              const textScale = 100;
+              text.setAttribute("look-at", "[gps-new-camera]");
+              text.setAttribute("scale", {
+                  x: textScale,
+                  y: textScale,
+                  z: textScale
+              });
+              text.setAttribute("value", feature.properties.name);
+              text.setAttribute("align", "center");
+              compoundEntity.appendChild(box);
+              compoundEntity.appendChild(text);
+              document.querySelector("a-scene").appendChild(compoundEntity);
           });
       }
       downloaded = true;
