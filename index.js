83438a717eb57ef883bc2290bed66906575d969a
steve-dickinson <steve.dickinson@defra.gov.uk>
2023-09-18 19:28:02 +0100

    update long lat

--- index.js
+++ index.js
@@ -13,12 +13,30 @@ function main() {
 
     const geom = new THREE.BoxGeometry(20, 20, 20);
     const mtl = new THREE.MeshBasicMaterial({color: 0xff0000});
-    const box = new THREE.Mesh(geom, mtl);
 
-    // Change this to a location 0.001 degrees of latitude north of you, so that you will face it
-    arjs.add(box, -0.72, 51.051); 
+    const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
 
-    // Start the GPS
+    let fetched = false;
+
+    // Handle the "gpsupdate" event on the LocationBased object
+    // This triggers when a GPS update (from the Geolocation API) occurs
+    // 'pos' is the position object from the Geolocation API.
+
+    arjs.on("gpsupdate", async(pos) => {
+        if(!fetched) {
+            const response = await fetch(`https://hikar.org/webapp/map?bbox=${pos.coords.longitude-0.01},${pos.coords.latitude-0.01},${pos.coords.longitude+0.01},${pos.coords.latitude+0.01}&layers=poi&outProj=4326`);
+
+            const geojson = await response.json();
+
+            geojson.features.forEach ( feature => {
+                const box = new THREE.Mesh(geom, mtl);
+                arjs.add(box, feature.geometry.coordinates[0], feature.geometry.coordinates[1]);            
+            });
+
+            fetched = true;
+        }
+    });
+
     arjs.startGps();
 
     requestAnimationFrame(render);
@@ -30,6 +48,10 @@ function main() {
             camera.aspect = aspect;
             camera.updateProjectionMatrix();
         }
+
+        // Update the scene using the latest sensor readings
+        deviceOrientationControls.update();
+
         cam.update();
         renderer.render(scene, camera);
         requestAnimationFrame(render);
